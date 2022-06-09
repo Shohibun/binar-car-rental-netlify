3a8f28ce63312c141204b2d9cc449490ef59e9cc
shohibun <shohibunnajam@gmail.com>
2022-06-09 14:19:57 +0700

    update package and anothers

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://server-binar.herokuapp.com/api/auth/";
 
 const register = (username, email, password) => {
   //Bakal return suatu object
